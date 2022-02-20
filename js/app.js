dca7162fcceb9831d72d6b6c0f1e4102fc62599b
Jonathan Joshua <jjoshua@stevens.edu>
2022-02-20 05:09:07 -0500

    created v.1.0 of project

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
-// METAMASK CONNECTION
-const TIMEOUT = 1000;
-const COLLECTION_NAME = 'CodeCats';
-let editions = [];
-let dots = 1;
+//Countdown Timer
+const clockdiv = document.getElementById("countdown");
+const countDownTime = new Date(
+  clockdiv.getAttribute("data-date")
+).getTime();
 
+const countdownfunction = setInterval(function () {
+  const now = new Date().getTime();
+  const diff = countDownTime - now;
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+  const minutes = Math.floor(diff % (1000 * 60 * 60) / (1000 * 60));
+  const seconds = Math.floor(diff % (1000 * 60) / 1000);
+
+  if (diff < 0) {
+    clockdiv.style.display = "none";
+    clearInterval(countdownfunction);
+  } else {
+    clockdiv.querySelector(".days").innerHTML = days;
+    clockdiv.querySelector(".hours").innerHTML = hours;
+    clockdiv.querySelector(".minutes").innerHTML = minutes;
+    clockdiv.querySelector(".seconds").innerHTML = seconds;
+  }
+}, 1000);
+
+
+// METAMASK CONNECTION
 window.addEventListener('DOMContentLoaded', () => {
   const onboarding = new MetaMaskOnboarding();
   const onboardButton = document.getElementById('connectWallet');
   let accounts;
 
-  const updateButton = async () => {
+  const updateButton = () => {
     if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
       onboardButton.innerText = 'Install MetaMask!';
       onboardButton.onclick = () => {
@@ -18,20 +39,18 @@ window.addEventListener('DOMContentLoaded', () => {
         onboarding.startOnboarding();
       };
     } else if (accounts && accounts.length > 0) {
-      onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+      onboardButton.innerText = `✔ ${accounts[0].slice(0,5)}...${accounts[0].slice(-4)}`;
       onboardButton.disabled = true;
       onboarding.stopOnboarding();
-      checkOwner(accounts[0]);
     } else {
-      onboardButton.innerText = 'Connect MetaMask!';
+      onboardButton.innerText = 'Connect Wallet';
       onboardButton.onclick = async () => {
         await window.ethereum.request({
           method: 'eth_requestAccounts',
         })
         .then(function(accounts) {
-          onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+          onboardButton.innerText = `✔ ${accounts[0].slice(0,5)}...${accounts[0].slice(-4)}`;
           onboardButton.disabled = true;
-          checkOwner(accounts[0]);
         });
       };
     }
@@ -45,89 +64,3 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-
-const checkOwner = async (account) => {
-  if(account) {
-    let isOwner = false;
-    let page = 1
-    
-    const data = await fetchWithRetry(`/.netlify/functions/isowner/?wallet=${account}&page=${page}`);
-
-    isOwner = !isOwner ? data.isOwner : isOwner;
-    updateStatusText(isOwner, true)
-    
-    editions = [...data.editions]
-    let nextPage = data.next_page
-
-    while(nextPage) {
-      page = nextPage
-      const data = await fetchWithRetry(`/.netlify/functions/isowner/?wallet=${account}&page=${page}`);
-
-      isOwner = !isOwner ? data.isOwner : isOwner;
-      updateStatusText(isOwner, true)
-      
-      editions = [...editions, ...data.editions]
-      nextPage = data.next_page
-    }
-
-    updateStatusText(isOwner, false)
-  }
-}
-
-function updateStatusText(isOwner, checking) {
-  const statusText = document.querySelector('.owner-status');
-  if(checking) {
-    if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
-    } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
-    }
-  } else {
-    if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
-    } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
-    }
-  }
-  dots = dots === 3 ? 1 : dots + 1;
-}
-
-function renderDots(dots) {
-  let dotsString = '';
-  for (let i = 0; i < dots; i++) {
-    dotsString += '.';
-  }
-  return dotsString;
-}
-
-function timer(ms) {
-  return new Promise(res => setTimeout(res, ms));
-}
-
-async function fetchWithRetry(url)  {
-  await timer(TIMEOUT);
-  return new Promise((resolve, reject) => {
-    const fetch_retry = (_url) => {
-      return fetch(_url).then(async (res) => {
-        const status = res.status;
-
-        if(status === 200) {
-          return resolve(res.json());
-        }            
-        else {
-          console.error(`ERROR STATUS: ${status}`)
-          console.log('Retrying')
-          await timer(TIMEOUT)
-          fetch_retry(_url)
-        }            
-      })
-      .catch(async (error) => {  
-        console.error(`CATCH ERROR: ${error}`)  
-        console.log('Retrying')    
-        await timer(TIMEOUT)    
-        fetch_retry(_url)
-      }); 
-    }
-    return fetch_retry(url);
-  });
-}
